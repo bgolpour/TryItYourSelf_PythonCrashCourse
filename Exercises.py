b8c8d0e9af36a52bdd92ca30053726f0d15562a3
Bax <b.golpour@gmail.com>
2020-03-24 17:38:05 -0400

    chapter 7th

--- Exercises.py
+++ Exercises.py
@@ -512,3 +512,25 @@ for user, user_info in people.items():
     loacation =  user_info['city']
     print("\n Full name: " + fullname.title())
     print("Location: " + loacation.title())
+
+### Page 121
+##7-1. Rental Car
+message = input("What kind of car you would like today?")
+print("\nLet me see if I can find you a Subaru.")
+
+##7-2. Restaurant Seating
+number = input("How many people are in your dinner table tonight?")
+number = int(number)
+if number >8:
+    print("You'll have to wait for a table.")
+else:
+    print("Your table is ready.")
+
+##7-3. Multiple of Ten
+number = input("Enter a number, and I'll tell you if it's multiple of 10 or not.")
+number = int(number)
+
+if number % 10 == 0:
+    print("\nThe number " + str(number) + " is a multiple of ten.")
+else:
+    print("\nThe number " + str(number) + " is not a multiple of ten.")
