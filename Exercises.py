9d0c7366eefd8fd2117a207a3efb18a413109f9f
Bax <b.golpour@gmail.com>
2020-04-10 19:21:14 -0400

    TheEndOfChapter10th

--- Exercises.py
+++ Exercises.py
@@ -1048,3 +1048,35 @@ except FileNotFoundError:
 
 else: 
     print("I know your favorite number! It's " + str(fav_number) + ".")
+## 10-13. Verify User
+import json
+def get_stored_username():
+    filename = 'username.json'
+    try:
+        with open(filename) as file_object:
+            username = json.load(file_object)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+def get_new_username():
+    username = input("What is your name? ")
+    filename = 'username.json'
+    with open(filename, 'w') as file_object:
+        json.dump(username, file_object)
+    return username
+
+def greet_user():
+    username = get_stored_username()
+    if username:
+        correct = input("Are you" + username + "? (y/n) ")
+        if correct == 'y':
+            print("Welcome back, " + username + "!")
+        else:
+            username = get_new_username()
+            print("We will remember you when you come back, " + username + "!")
+    else:
+        username = get_new_username()
+        print("We'll remember you when you come back, " + username + "!")
+greet_user()
